79b055c488c97582ec81ffbea58c87815a9ab75c
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2023-11-27 10:41:20 -0800

    added firebase

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -28,6 +28,9 @@ const Navigation = () => {
                     <Link className="nav-link" to='/shop'>
                         GIFT GUIDE
                     </Link>
+                    <Link className="nav-link" to='/sign-in'>
+                        SIGN IN
+                    </Link>
                 </div>
             </div>
             <Outlet />
