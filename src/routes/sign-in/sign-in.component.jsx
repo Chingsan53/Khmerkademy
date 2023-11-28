c746440a8400cf6e674aca779b7474e8f75d897c
Chingsan53 <89380886+Chingsan53@users.noreply.github.com>
2023-11-28 15:07:24 -0800

    added sign in

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,10 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../components/utils/firebase/firebase.utils';
+
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth } from '../../components/utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -10,8 +14,9 @@ const SignIn = () => {
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
-                Sign In
+                Sign In with Google Popup
             </button>
+            <SignUpForm />
         </div>
     );
 };
