30a4c44fe10cdc31263b12428d071eb7ea374728
Chingsan53 <cly53@csu.fullerton.edu>
2023-11-30 21:04:44 -0800

    added shopping cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,10 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
+
 import { ReactComponent as FunkoLogo } from "../../assets/Funko.svg";
 import { signOutUser } from "../../components/utils/firebase/firebase.utils";
 import { UserContext } from "../../contexts/user.context";
@@ -8,44 +12,46 @@ import { UserContext } from "../../contexts/user.context";
 import "./navigation.style.scss";
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
-
-    return (
-        <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to="/">
-                    <FunkoLogo className="logo" />
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-link" to="/shop">
-                        SHOP
-                    </Link>
-                    <Link className="nav-link" to="/shop">
-                        CATEGORY
-                    </Link>
-                    <Link className="nav-link" to="/shop">
-                        FEATURED
-                    </Link>
-                    <Link className="nav-link" to="/shop">
-                        POP! YOURSELF
-                    </Link>
-                    <Link className="nav-link" to="/shop">
-                        GIFT GUIDE
-                    </Link>
-                    {currentUser ? (
-                        <span className='nav-link' onClick={signOutUser}>
-                            SIGN OUT
-                        </span>
-                    ) : (
-                        <Link className='nav-link' to='/auth'>
-                            SIGN IN
-                        </Link>
-                    )}
-                </div>
-            </div>
-            <Outlet />
-        </Fragment>
-    );
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <FunkoLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/shop">
+            CATEGORY
+          </Link>
+          <Link className="nav-link" to="/shop">
+            FEATURED
+          </Link>
+          <Link className="nav-link" to="/shop">
+            POP! YOURSELF
+          </Link>
+          <Link className="nav-link" to="/shop">
+            GIFT GUIDE
+          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
+          <CartIcon />
+        </div>
+        {isCartOpen && <CartDropdown />}
+      </div>
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Navigation;
