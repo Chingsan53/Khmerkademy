5295c858ddc332620ad12737225925f347d5022f
Chingsan53 <cly53@csu.fullerton.edu>
2023-12-04 16:47:23 -0800

    add some styling

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -15,10 +15,14 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="fotter">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        {/* <span className="price">{price}</span> */}
+      </div>
+      <div className="fotter">
+        {/* <span className="name">{name}</span> */}
+        <span className="price">${price}</span>
       </div>
       <Button buttonType="inverted" onClick={addProductToCart}>
-        Add to cart
+        Add
       </Button>
     </div>
   );
