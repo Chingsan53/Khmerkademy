e9c5272a82e71644b01f2270361b0d0381474488
Chingsan53 <cly53@csu.fullerton.edu>
2023-12-06 18:56:11 -0800

    directory-item

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -10,7 +10,7 @@ export const CategoriesProvider = ({ children }) => {
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments("categories");
+      const categoryMap = await getCategoriesAndDocuments();
       setCategoriesMap(categoryMap);
       console.log(categoryMap);
     };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import "./shop.styles.scss";
+import Category from "../category/category.component";
 import { Routes, Route } from "react-router-dom";
 
 const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=":category" element={<category />} />
+      <Route path=":category" element={<Category />} />
     </Routes>
   );
 };
